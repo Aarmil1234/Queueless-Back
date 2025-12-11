34987e799104ec37f9fb871b03f9a139848a2324
Aarmil Sadarani <aarmilsadarani981@gmail.com>
2025-12-12 00:58:13 +0530

    db change

--- app/connection/db.js
+++ app/connection/db.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 
 // Replace this with your MongoDB URI
-const uri = 'mongodb+srv://snselftech:rDa9TicNjJJVCZcD@selftech.7ie4n.mongodb.net/?retryWrites=true&w=majority&appName=selftech/medicare'; // or your remote URI
+// const uri = 'mongodb+srv://snselftech:rDa9TicNjJJVCZcD@selftech.7ie4n.mongodb.net/?retryWrites=true&w=majority&appName=selftech/medicare'; // or your remote URI
+const uri = 'mongodb+srv://aarmilsadarani981_db_user:Aarmil@123@cluster0.rtyncjh.mongodb.net/?appName=Cluster0'
 
 // const uri = 'mongodb://localhost:27017/medicare'; // or your remote URI
 
